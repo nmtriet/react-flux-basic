0efc3f5a9787ce3a95659413921580899882fc90
nmtriet <minhtriet1989@gmail.com>
2016-07-02 12:40:24 +0700

    Fix bug

--- src/js/actions/AppAction.js
+++ src/js/actions/AppAction.js
@@ -2,7 +2,7 @@ import AppDispatcher from '../dispatchers/AppDispatcher';
 
 export default {
     addItem: (text) => {
-        console.log('Login action dispatches to Store');
+        console.log('Add action dispatches to Store');
 
         AppDispatcher.dispatch({
             actionType: 'ADD_ACTION',
